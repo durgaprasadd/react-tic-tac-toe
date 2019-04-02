2503ff0cb2a874f0801e255b65baca7370a254a9
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2019-04-02 16:15:35 +0530

    Updates

---
+++ precache-manifest.9530fd0ac763c2b3f638ce282098c876.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "25a6b97fc19d90dc00eb",
+    "url": "/static/js/main.54e9b4f1.chunk.js"
+  },
+  {
+    "revision": "33de82a2476455957ce7",
+    "url": "/static/js/2.a1b4b493.chunk.js"
+  },
+  {
+    "revision": "25a6b97fc19d90dc00eb",
+    "url": "/static/css/main.c9023edf.chunk.css"
+  },
+  {
+    "revision": "dded4b858ac4be00f080792bc573d8bf",
+    "url": "/index.html"
+  }
+];
